aae30a44e161d0a6b1341b0662ae23d7a6dafbc3
sep3r <sep3r@yahoo.com>
2024-11-15 17:47:13 +0330

    feat(new-archive): added new archive

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import ListEmployeeComponents from './components/ListEmployeeComponent';
+
+function App() {
+  return (
+      <ListEmployeeComponents />
+  );
+}
+
+export default App;
